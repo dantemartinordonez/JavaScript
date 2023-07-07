78d65e3efad2860de0e3df09103fe3c5f87e8daf
ordoñez dante martin <dantemartinordonez@hotmail.com>
2023-07-07 15:26:50 -0300

    armado de programa con js para veriificacion de edad

--- index.js
+++ index.js
@@ -1,60 +1,11 @@
-// let precio = parseInt(prompt("ingrese su numero"));
+alert("Bienvenidos para comenzar vamos a corroborar tu mayoria de edad");
 
-// if (precio < 20) {
-//     console.log("el precio es menor que 20");
-// }
-// else if (precio < 50){
-//     console.log("el precio es menor que 50");
-// }
-// else if (precio <100){
-//     console.log("el precio es menor que 100");
-// }
-// else {
-//     console.log("el precio es mayor que 100");
-// }
+let edad = parseInt(prompt("Ingrese su edad"));
+let edadMinima = 18;
 
-
-// ejemplo de edad:
-
-
-// let edad = parseInt(prompt("ingrese su edad"));
-// if (edad < 18){
-//     console.log("usted es menor de edad");
-// }else if (edad >= 18){
-//     console.log("usted es mayor de edad");
-// }
-
-//operadores logicos ==
-//sin un valor es igual al otro o no
-
-// let numero1 = 20;
-// let numero2 = 20;
-
-// let resultado = numero1 == numero2
-// console.log(resultado);
-
-
-
-//operadores logicos === extrictamente igual
-
-
-let numero1 = 20;
-let numero2 = 30;
-// let numeroTexto = 20
-
-// let resultado = numero1 === numeroTexto;
-// console.log(resultado);
-
-//operador != significa que es distinto
-//operador !== significa que es extrictamente distinto
-//operador < menor
-//operador > mayor
-//operador <= menor o igual
-//operador >= mayor o igual
-//operador && para colocar dos condiciones.
-//if(numero 1 > numero2) && (numero1 >numero3)
-//operador ││
-
-let resultado = numero1 >= numero2
-console.log(resultado);
-
+if (edad >= edadMinima) {
+  alert("Bienvenido puede ingresar");
+} else if (edad <= edadMinima) {
+  alert("No cumple con la edad mínima para ingresar");
+  alert(actualizar)
+}

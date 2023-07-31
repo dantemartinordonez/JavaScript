f63b371694bd5fd28a2c8dcb9ce892153b331460
ordoñez dante martin <dantemartinordonez@hotmail.com>
2023-07-31 19:05:47 -0300

    pruebas segundas preentrega

--- index.js
+++ index.js
@@ -1,109 +1,128 @@
-alert("bienvenido a control de sotok de vehiculos 0km Multi-car")
+//un simulador para un empleado de una concesionaria de automores multimarcas 0km donde puede agregar quitar o cambiar precios de los vehiculos.
+alert("Bienvenido a Control de Stock de Vehículos 0km Multi-car");
 
-const calcularPromedio = (numeros) => {
-  if (numeros.length === 0) {
-    return 0;
+
+
+class Vehiculo {
+  constructor(marca, modelo, stock, precio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.stock = stock;
+    this.precio = precio;
   }
+}
 
-  let suma = 0;
-  for (const numero of numeros) {
-    suma += numero;
-  }
-  return suma / numeros.length;
-};
 
-// Array de autos y stocjk dispionibles
+
+// Array de autos disponibles con stock y precios 
 const autos = [
-  { marca: 'Peugeot', modelo: '208', stock: 5, precio: 9000000 },
-  { marca: 'Fiat', modelo: 'Cronos', stock: 3, precio: 8500000 },
-  { marca: 'Ford', modelo: 'Focus', stock: 7, precio: 12000000 },
-  { marca: 'Fiat', modelo: 'Argo', stock: 8, precio: 8000000 },
+  new Vehiculo('Peugeot', '208', 5, 9000000),
+  new Vehiculo('Fiat', 'Cronos', 3, 8500000),
+  new Vehiculo('Ford', 'Focus', 7, 12000000),
+  new Vehiculo('Fiat', 'Argo', 8, 8000000),
 ];
 
-//Funcion para mostrar el precio y stock de autos disponibles en la concesionaria
-const mostrarStockYPrecio = () => {
-  let mensaje = 'Stock y precio de autos:\n';
+
+
+
+const mostrarAutosDisponibles = () => {
+  let mensaje = 'MARCA, MODELO, STOCK Y PRECIOS DE AUTOS DISPONIBLES:\n';
   autos.forEach((auto) => {
     mensaje += `${auto.marca} ${auto.modelo}: ${auto.stock} unidades - Precio: $${auto.precio}\n`;
   });
   alert(mensaje);
 };
-//agregar vehículos al stock
-const agregarVehiculos = () => {
-  const marca = prompt('Ingrese la marca del nuevo auto a ingresar:');
-  const modelo = prompt('Ingrese el tipo de modelo del vehiculo:');
-  const cantidad = parseInt(prompt('coloque la cantidad de vehiculos que ingresan al stock:'));
 
-  let autoEncontrado = false;
-  for (let i = 0; i < autos.length; i++) {
-    if (autos[i].marca.toLowerCase() === marca.toLowerCase() && autos[i].modelo.toLowerCase() === modelo.toLowerCase()) {
-      autos[i].stock += cantidad;
-      autoEncontrado = true;
-      break;
-    }
-  }
-
-  if (!autoEncontrado) {
-    const precio = parseFloat(prompt('Ingrese el precio del vehiculo:'));
-    autos.push({ marca, modelo, stock: cantidad, precio });
-  }
-
-  alert(`Se han agregado ${cantidad} unidades de ${marca} ${modelo} al stock.`);
+// encontrar vehículo en el array autos por marca y modelo
+const encontrarIndiceVehiculo = (marca, modelo) => {
+  return autos.findIndex(
+    (auto) => auto.marca.toLowerCase() === marca.toLowerCase() && auto.modelo.toLowerCase() === modelo.toLowerCase()
+  );
 };
 
-//quitar vehículos del stock
-const quitarVehiculos = () => {
-  const marca = prompt('Ingrese la marca del vehículo a quitar del stock:');
-  const modelo = prompt('Ingrese el modelo del vehículo a quitar del stock:');
-  const cantidad = parseInt(prompt('Ingrese la cantidad de unidades que desea quitar:'));
 
-  let autoEncontrado = false;
-  for (let i = 0; i < autos.length; i++) {
-    if (autos[i].marca.toLowerCase() === marca.toLowerCase() && autos[i].modelo.toLowerCase() === modelo.toLowerCase()) {
-      if (autos[i].stock >= cantidad) {
-        autos[i].stock -= cantidad;
-        autoEncontrado = true;
-        alert(`Se han quitado ${cantidad} unidades de ${marca} ${modelo} del stock.`);
-      } else {
-        alert(`No se pueden quitar ${cantidad} unidades de ${marca} ${modelo} porque no hay suficiente stock.`);
-      }
-      break;
-    }
+
+// agregar autos al stock
+const agregarVehiculos = () => {
+  const marca = prompt('Ingrese la marca del nuevo vehiculo a ingresar:');
+  const modelo = prompt('Ingrese el tipo de modelo del vehiculo:');
+  const cantidad = parseInt(prompt('Coloque la cantidad de vehiculos que ingresan al stock:'), 10);
+
+  const indiceVehiculo = encontrarIndiceVehiculo(marca, modelo);
+  if (indiceVehiculo !== -1) {
+    autos[indiceVehiculo].stock += cantidad;
+    alert(`Se han agregado ${cantidad} unidades de ${marca} ${modelo} al stock.`);
+  } else {
+    const precio = parseFloat(prompt('Ingrese el precio del vehículo:'));
+    autos.push(new Vehiculo(marca, modelo, cantidad, precio));
+    alert(`Se han agregado ${cantidad} unidades de ${marca} ${modelo} al stock.`);
   }
+};
+// quitar vehículos del stock
+const quitarVehiculos = () => {
+  const marca = prompt('Ingrese la marca del vehiculo a quitar del stock:');
+  const modelo = prompt('Ingrese el modelo del vehiculo a quitar del stock:');
+  const cantidad = parseInt(prompt('Ingrese la cantidad de unidades que desea quitar:'), 10);
 
-  if (!autoEncontrado) {
+  const indiceVehiculo = encontrarIndiceVehiculo(marca, modelo);
+  if (indiceVehiculo !== -1) {
+    if (autos[indiceVehiculo].stock >= cantidad) {
+      autos[indiceVehiculo].stock -= cantidad;
+      alert(`Se han quitado ${cantidad} unidades de ${marca} ${modelo} del stock.`);
+    } else {
+      alert(`No se pueden quitar ${cantidad} unidades de ${marca} ${modelo} porque no hay suficiente stock.`);
+    }
+  } else {
     alert(`El vehículo ${marca} ${modelo} no se encuentra en el stock.`);
   }
 };
+// cambio de precio de un auto existente
+const cambiarPrecio = () => {
+  const marca = prompt('Ingrese la marca del vehiculo para cambiar el precio:');
+  const modelo = prompt('Ingrese el modelo del vehiculo para cambiar el precio:');
 
-//stock y preco
-mostrarStockYPrecio();
-
-// hacemos un ciclo para que el operario pueda salir de la funcion
+  const indiceVehiculo = encontrarIndiceVehiculo(marca, modelo);
+  if (indiceVehiculo !== -1) {
+    const precioAnterior = autos[indiceVehiculo].precio;
+    alert(`El precio actual de ${marca} ${modelo} es: $${precioAnterior}`);
+    
+    const nuevoPrecio = parseFloat(prompt('Ingrese el nuevo precio del vehiculo:'));
+    autos[indiceVehiculo].precio = nuevoPrecio;
+    alert(`El precio de ${marca} ${modelo} ha sido actualizado a $${nuevoPrecio}.`);
+  } else {
+    alert(`El vehiculo ${marca} ${modelo} no se encuentra en el stock.`);
+  }
+};
+mostrarAutosDisponibles();
+// opciones para el usuario
 let continuar = true;
 while (continuar) {
-  const opcion = prompt('Escriba la accion deseea realizar:\n1 - Agregar \n2 - Quitar \n3 - Salir').toLowerCase();
+  const opcion = prompt('Escriba la acción que desea realizar:\n1 - Agregar vehiculo \n2 - Quitar vehiculo \n3 - Cambiar Precio \n4 - Salir').toLowerCase();
 
   switch (opcion) {
+    case '1':
     case 'agregar':
       agregarVehiculos();
       break;
+    case '2':
     case 'quitar':
       quitarVehiculos();
       break;
+    case '3':
+    case 'cambiar precio':
+      cambiarPrecio();
+      break;
+    case '4':
     case 'salir':
       continuar = false;
       break;
     default:
-      alert('Opción no válida. Por favor, escriba "agregar", "quitar" o "salir".');
+      alert('Opción no valida. Por favor, coloque la opcion numerica del 1 al 4');
   }
 
-  // Mostramos el stock y precio actualizado
-  mostrarStockYPrecio();
+  mostrarAutosDisponibles();
 }
 
-let totalStock = 0;
-for (let i = 0; i < autos.length; i++) {
-  totalStock += autos[i].stock;
-}
-console.log(`El total de productos en stock es: ${totalStock} unidades.`);
+// console.log del total del stock del total de los autos en la concesionaria
+const totalStock = autos.reduce((total, auto) => total + auto.stock, 0);
+console.log(`El total de autos en stock es: ${totalStock} unidades.`);
